270681db01a5c91439880a3c63eef28c727a9be5
sripwoud <me@sripwoud.xyz>
2025-02-20 14:26:08 +0100

    feat(templates): add circom template

--- templates/circom/web/src/main.tsx
+++ templates/circom/web/src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import './global.css'
 
+// biome-ignore lint/style/noNonNullAssertion: index.html contains div#root
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
