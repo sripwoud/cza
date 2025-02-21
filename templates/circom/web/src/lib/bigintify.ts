4479d5b0d0239fad5d3ac98d5ada1458845d3e58
sripwoud <me@sripwoud.xyz>
2025-02-21 23:24:25 +0100

    chore: update dev task

--- templates/circom/web/src/lib/bigintify.ts
+++ templates/circom/web/src/lib/bigintify.ts
@@ -1,3 +1,2 @@
 // @ts-expect-error can't determine recursive type
-export const bigintify = (obj: string | string[] | string[][]) =>
-  Array.isArray(obj) ? obj.map(bigintify) : BigInt(obj);
+export const bigintify = (obj: string | string[] | string[][]) => Array.isArray(obj) ? obj.map(bigintify) : BigInt(obj)
